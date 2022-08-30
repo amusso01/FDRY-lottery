63536e8bb71a220f319600a2e5cc43b4e607feaf
amusso01 <eandram@hotmail.it>
2022-08-30 10:45:55 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: FDRY lottery
+ * Plugin URI: https://github.com/amusso01/FDRY-lottery
+ * Description: Create a win ticket based on woocommerce oredr ID
+ * Version: 1.0
+ * Author: Andrea Musso
+ * Author URI: https://github.com/amusso01
+ */
